ca34d9637c227e0ada1f94acbdc9587518ea129f
Allison Ause <allison.ause@gmail.com>
2022-09-21 14:54:01 -0700

    created loading state and wired up to Main.js and usePokemon hook with timeout

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,10 +4,11 @@ import usePokemon from '../../hooks/usePokemon.js';
 import PokeList from '../PokeList/PokeList.js';
 
 export default function Main() {
-  const pokemonList = usePokemon();
+  const { pokemonList, loading } = usePokemon();
+  if (loading) return <div className="loader"></div>;
   return (
-    <div className='main'>
-      <PokeList pokemonList={pokemonList}/>
+    <div className="main">
+      <PokeList pokemonList={pokemonList} />
     </div>
   );
 }
--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -7,12 +7,16 @@ import { fetchPokemon } from '../services/pokemon.js';
 // provides data from API
 export default function usePokemon() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await fetchPokemon();
         setPokemonList(data);
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
       } catch (e) {
         /* eslint-disable-next-line no-console */
         console.error(e);
@@ -20,6 +24,5 @@ export default function usePokemon() {
     };
     fetchData();
   }, []);
-
-  return pokemonList;
+  return { pokemonList, loading };
 }
