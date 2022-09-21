bcae3da59d74a9dd15ff9ff3b1b93ce75eb78e79
olivia pasion <oap9820@gmail.com>
2022-09-21 13:03:55 -0700

    finished usePokemon hook

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,10 @@
+import usePokemon from '../../hooks/usePokemon.js';
 
 export default function Main() {
+  const pokemon = usePokemon();
   return (
     <div className='main'>
+      <div>{pokemon}</div>
     </div>
   );
 }
--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -1,7 +1,26 @@
 // react
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // services
 import { fetchPokemon } from '../services/pokemon.js';
 
+// provides data from API
+export default function usePokemon() {
+  const [pokemon, setPokemon] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchPokemon();
+        setPokemon(data);
+        console.log(pokemon);
+      } catch (e) {
+        /* eslint-disable-next-line no-console */
+        console.error(e);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return pokemon;
+}
