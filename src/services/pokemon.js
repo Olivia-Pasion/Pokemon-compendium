f6d7a6fd1181b1af4182837aeed64187388f799e
Allison Ause <allison.ause@gmail.com>
2022-09-21 15:05:17 -0700

    limited fetch to 10 pokemon for Pokelist display

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,8 +1,9 @@
-
 export async function fetchPokemon() {
+  const params = new URLSearchParams();
+  params.set('perPage', 10);
 
   const resp = await fetch(
-    `https://pokedex-alchemy.herokuapp.com/api/pokedex`
+    `https://pokedex-alchemy.herokuapp.com/api/pokedex?${params.toString()}`
   );
   const data = await resp.json();
   return data.results;
