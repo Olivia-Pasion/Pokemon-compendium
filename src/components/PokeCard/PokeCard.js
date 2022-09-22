4f7e7513e757413f360509de1c8971fcb9bad938
olivia pasion <oap9820@gmail.com>
2022-09-22 15:13:24 -0700

    styling to PokeCard spans, hide type2 span if NA

--- src/components/PokeCard/PokeCard.js
+++ src/components/PokeCard/PokeCard.js
@@ -1,14 +1,17 @@
 import './PokeCard.css';
 
 export default function PokeCard({ url_image, pokemon, type_1, type_2, hp }) {
+  let type2;
+  if (type_2 !== 'NA') type2 = (<span className="type2">{type_2[0].toUpperCase() + type_2.substring(1)}</span>);
   return (
     <div className="poke-card">
       <img src={url_image} />
-      <h3>{pokemon}</h3>
+      <h3>{pokemon[0].toUpperCase() + pokemon.substring(1)}</h3>
       <div className="info">
-        <span className="type1">{type_1}</span>
-        <span className="type2">{type_2}</span>
-        <span className="hp">{hp}</span>
+        <span className="type1">{type_1[0].toUpperCase() + type_1.substring(1)}</span>
+        {type2}
+        <br></br>
+        <span className="hp">Starting HP {hp}</span>
       </div>
     </div>
   );
