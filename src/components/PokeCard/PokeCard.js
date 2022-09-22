46d12a6eb89d5ce34b3aae0da2052f31d7f9c65f
Allison Ause <allison.ause@gmail.com>
2022-09-22 16:43:04 -0700

    added bounce styling to pokemon on hover

--- src/components/PokeCard/PokeCard.js
+++ src/components/PokeCard/PokeCard.js
@@ -2,10 +2,13 @@ import './PokeCard.css';
 
 export default function PokeCard({ url_image, pokemon, type_1, type_2, hp }) {
   let type2;
-  if (type_2 !== 'NA') type2 = (<span className="type2">{type_2[0].toUpperCase() + type_2.substring(1)}</span>);
+  if (type_2 !== 'NA')
+    type2 = <span className="type2">{type_2[0].toUpperCase() + type_2.substring(1)}</span>;
   return (
     <div className="poke-card">
-      <img src={url_image} />
+      <div className="poke-img bounce-5">
+        <img src={url_image} />
+      </div>
       <h3>{pokemon[0].toUpperCase() + pokemon.substring(1)}</h3>
       <div className="info">
         <span className="type1">{type_1[0].toUpperCase() + type_1.substring(1)}</span>
