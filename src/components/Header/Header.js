7037201dddd0e84899f0a383f3b7e2d7a6ccbcb3
olivia pasion <oap9820@gmail.com>
2022-09-21 17:35:30 -0700

    worked on header CSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,10 @@
+import './Header.css';
 export default function Header() {
   return (
-    <div>
+    <header>
+      <img className='left-image' src='/assets/snorlax-peeking.png'></img>
       <h1>Pokemon Stable!</h1>
-    </div>
+      <img className='right-image' src='/assets/pikachu-peeking.png'></img>
+    </header>
   );
 }
