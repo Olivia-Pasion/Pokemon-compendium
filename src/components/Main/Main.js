e9b887aa1dc1f657a978e109405706384931fb0a
Allison Ause <allison.ause@gmail.com>
2022-09-21 17:04:03 -0700

    added Header component with title display

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,16 +3,25 @@ import './Main.css';
 import usePokemon from '../../hooks/usePokemon.js';
 
 // component imports
+import Header from '../Header/Header.js';
 import Filter from '../Filter/Filter.js';
 import PokeList from '../PokeList/PokeList.js';
 import Search from '../Search/Search';
 
 export default function Main() {
-  const { pokemonList, loading, types, selectedType, setSelectedType, setSearchTerm } = usePokemon();
+  const {
+    pokemonList,
+    loading,
+    types,
+    selectedType,
+    setSelectedType,
+    setSearchTerm,
+  } = usePokemon();
   if (loading) return <div className="loader"></div>;
   return (
     <div className="main">
-      <Filter types={types} selectedType={selectedType} setSelectedType={setSelectedType}/>
+      <Header />
+      <Filter types={types} selectedType={selectedType} setSelectedType={setSelectedType} />
       <Search setSearchTerm={setSearchTerm} />
       <PokeList pokemonList={pokemonList} />
     </div>
