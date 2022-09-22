2654c935aa8212113a0427ea6c62955472832af9
Allison Ause <allison.ause@gmail.com>
2022-09-22 14:40:28 -0700

    added background image and styling to the controls component

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,11 +1,17 @@
 import Filter from '../Filter/Filter';
 import Search from '../Search/Search';
+import './Controls.css';
+import background from '../../controls_background.png';
 
 export default function Controls({ types, selectedType, setSelectedType, setSearchTerm }) {
   return (
-    <div>
-      <Filter types={types} selectedType={selectedType} setSelectedType={setSelectedType} />
-      <Search setSearchTerm={setSearchTerm} />
+    <div className="controls" style={{ backgroundImage: `url(${background})` }}>
+      <div className="filter-component">
+        <Filter types={types} selectedType={selectedType} setSelectedType={setSelectedType} />
+      </div>
+      <div className="search-component">
+        <Search setSearchTerm={setSearchTerm} />
+      </div>
     </div>
   );
 }
